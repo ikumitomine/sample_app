8f5ae0a090246bf802f0b7bca0195dcb358865f0
東峰いくみ <ikumitomine@touhouiuminombp.lan>
2020-08-17 18:26:53 +0900

    [Add]3-7画像投稿機能

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '7238fe2198c32f41dc14532da98efddff4d70ed290ade7c996a112de87446a0223349d4ceb1fa4c436e6de6685f84b3b26735baf00331d67e4ac9133e3eb5e2a'
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_16_060420) do
+ActiveRecord::Schema.define(version: 2020_08_17_073812) do
 
   create_table "lists", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "title"
     t.string "body"
+    t.string "image_id"
   end
 
 end
